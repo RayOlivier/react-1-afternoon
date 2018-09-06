e723e879eb9fabf511861e68e171eda20be93529
Ray Olivier <rayolivier@outlook.com>
2018-09-06 15:04:22 -0500

    finished evens and odds

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+
+class EvenAndOdd extends Component{
+    constructor(){
+        super();
+        
+        this.state ={
+            evenArray: [],
+            oddArray: [],
+            userInput: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(x){
+        this.setState({userInput:x.target.value})
+    }
+
+    handleClick(x){
+        const{userInput}=this.state;
+        let newArr = userInput.split(',');
+
+        let arrEvens =[];
+        let arrOdds=[];
+
+        newArr.forEach((e,i,arr)=>{
+            if(e%2===0){
+                arrEvens.push(parseInt(e,10));
+            }
+            else{
+                arrOdds.push(parseInt(e,10));
+            }
+        })
+        console.log(arrEvens);
+        this.setState({evenArray: arrEvens});
+        this.setState({oddArray: arrOdds});
+        console.log(this.state.evenArray)
+    }
+
+
+
+    render(){
+        console.log(this.state.userInput);
+
+        return(
+            <div  className="puzzleBox evenAndOddPB">
+                <h4>Evens and Odds</h4>
+                <input className="inputLine"
+                onChange={(event)=> this.handleChange(event)}
+                ></input>
+                <button className="confirmationButton"
+                onClick={this.handleClick}
+                
+                >Run</button>
+                <span className='resultsBox'>Evens: {JSON.stringify(this.state.evenArray)}</span>
+                <span className='resultsBox'>Odds: {JSON.stringify(this.state.oddArray)}</span>
+
+                
+
+
+            </div>
+        )
+    }
+}
+
+
+export default EvenAndOdd;
